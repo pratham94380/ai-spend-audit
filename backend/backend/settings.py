521927685852793875923c74337ddc153344ea89
pratham94380 <prathamrajulwar@gmail.com>
2026-05-12 16:59:45 +0530

    fix: allow deployed backend hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@f%bbw4h2*2y6gpema%brysl-hvq$o^#l^tv5@szu$!)f*pz_3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
